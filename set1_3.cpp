537188619adaab7aa012871550d7f366d9878402
MonishaEM <37539573+MonishaEM@users.noreply.github.com>
2018-03-20 21:38:32 +0530

    Create set1_3.cpp

---
+++ set1_3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char s;
+	cin>>s;
+	if(s=='a' && 'e' && 'i' && 'o' && 'u')
+	{
+		cout<<"Vowel";
+	}
+	else
+	{
+		cout<<"Consonant";
+	}
+	// your code goes here
+	return 0;
+}
