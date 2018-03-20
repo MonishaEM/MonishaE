34cf95b996ef98425338da10aa806af92fc7758a
MonishaEM <37539573+MonishaEM@users.noreply.github.com>
2018-03-20 17:30:53 +0530

    Create set1_1.cpp

---
+++ set1_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	if(n>0)
+	{
+	cout<<"Positive";	
+	}
+	if(n<0)
+	{
+	cout<<"Negative";
+	}
+	if(n==0)
+	{
+	cout<<"Zero";
+	}
+	// your code goes here
+	return 0;
+}
