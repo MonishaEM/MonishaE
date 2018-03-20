f84db6dc5ea5b973d3d8aee2de0d482a3abfbe5e
MonishaEM <37539573+MonishaEM@users.noreply.github.com>
2018-03-20 17:41:50 +0530

    Create set1_2.cpp

---
+++ set1_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	int n;
+	cin>>n;
+	if(n%2==0)
+	{
+	cout<<"Number is even";
+	}
+	if(n%2!=0)
+	{
+	cout<<"Number is odd";
+	}
+	// your code goes here
+	return 0;
+}
