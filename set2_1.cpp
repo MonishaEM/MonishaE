39009e0f4ff7d723397b509c3a1a23e86c49e39a
MonishaEM <37539573+MonishaEM@users.noreply.github.com>
2018-03-21 17:53:57 +0530

    Create set2_1.cpp

---
+++ set2_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	int num,p;
+	cin>>num;
+	cin>>p;
+	int result=pow(num,p);
+	cout<<result;
+	// your code goes here
+	return 0;
+}
