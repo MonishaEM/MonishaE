7e9f35a7831a1a7b81664458da835d7c2b714135
MonishaEM <37539573+MonishaEM@users.noreply.github.com>
2018-03-27 22:36:02 +0530

    Create set2_5.cpp

---
+++ set2_5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n1,n2;
+	cin>>n1;
+	cin>>n2;
+	for(int i=n1;i<=n2;i++)
+	{
+		if(i%2==0)
+		{
+			cout<<i<<" ";
+		}
+	}
+	// your code goes here
+	return 0;
+}
