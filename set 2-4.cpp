ae702853535b0f196420448d3f953943141cae5b
MonishaEM <37539573+MonishaEM@users.noreply.github.com>
2018-03-27 12:31:05 +0530

    Create set 2-4.cpp

---
+++ set 2-4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n1,n2;
+	cin>>n1;
+	cin>>n2;
+	for(int i=n1;i<=n2;i++)
+	{
+		if(i%2!=0)
+		{
+			cout<<i<<" ";
+		}
+  }  
+	return 0;
+}
