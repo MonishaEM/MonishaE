977217deeab7d9891700fd1455fb72a0c5538942
MonishaEM <37539573+MonishaEM@users.noreply.github.com>
2018-04-05 15:27:58 +0530

    Create set3_5.cpp

---
+++ set3_5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	int *a=new int[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				swap(a[j],a[j+1]);
+			}
+		}
+	}
+	cout<<a[n/2];
+	return 0;
+}
